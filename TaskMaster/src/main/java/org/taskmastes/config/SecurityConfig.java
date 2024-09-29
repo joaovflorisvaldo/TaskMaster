3dbd08309c01a5779210fb4d137d1810c4e06238
joaovflorisvaldo <joaovflorisvaldo@gmail.com>
2024-09-29 16:50:49 -0300

    Fixed layout bug

--- TaskMaster/src/main/java/org/taskmastes/config/SecurityConfig.java
+++
@@ -1,8 +0,0 @@
-package org.taskmastes.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class SecurityConfig {
-    // Seu código atual (desativado para ignorar as configurações)
-}
--- TaskMaster/src/main/java/org/taskmastes/repository/HabitoHistoricoRepository.java
+++
@@ -1,9 +0,0 @@
-package org.taskmastes.repository;
-
-import org.taskmastes.model.HabitoHistorico;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface HabitoHistoricoRepository extends JpaRepository<HabitoHistorico, Long> {
-}
